6a72244f74385c7c634a699f54c1941ee1366f84
EvanLRay <Evan.L.Ray@gmail.com>
2016-02-16 15:19:25 -0500

    Update to writeup with some plots summarizing results of application to influenza prediction

--- inst/article/knit_article.R
+++ inst/article/knit_article.R
@@ -1,4 +1,4 @@
-setwd("/media/evan/data/Reich/infectious-disease-prediction-with-kcde")
+setwd("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/inst/article")
 library(knitr)
-knit2pdf("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/infectious-disease-prediction-with-kcde.Rnw")
+knit2pdf("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/inst/article/infectious-disease-prediction-with-kcde.Rnw")
 
