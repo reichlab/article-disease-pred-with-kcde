b9d215b1f7ee6689279d0d0caa630aeccb57a6ab
EvanLRay <Evan.L.Ray@gmail.com>
2016-06-10 14:22:11 -0400

    miscellaneous tex-related files for article and supplement that I forgot to commit before

---
+++ inst/article-supplement/knit_article_supplement.R
@@ -0,0 +1,4 @@
+setwd("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/inst/article-supplement")
+library(knitr)
+knit2pdf("/media/evan/data/Reich/infectious-disease-prediction-with-kcde/inst/article-supplement/infectious-disease-prediction-with-kcde-SUPPLEMENT.Rnw")
+
